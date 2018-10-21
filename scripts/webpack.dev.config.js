1ba17b51b68f8a3cbde2f32abe2064585c4f950d
Henry Li <henry1943@163.com>
2018-10-21 12:48:52 +0800

    Add react and global less

--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -5,7 +5,17 @@ const baseConfig = require('./webpack.base.config');
 module.exports = merge(
   {
     mode: 'development',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.jsx?$/,
+          use: ['eslint-loader'],
+          exclude: /node_modules/
+        }
+      ]
+    }
   },
   baseConfig
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
 import '@babel/polyfill';
-import 'normalize.css/normalize.css';
+import React from 'react';
+import { render } from 'react-dom';
 
+import 'normalize.css/normalize.css';
 import './index.less';
+
+render(<div>Henry</div>, document.getElementById('x-drive-mount-point'));
